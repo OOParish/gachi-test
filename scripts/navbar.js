27fb9e6f3e7d24b107615dbba372b940ab902e90
OOParish <91882595+OOParish@users.noreply.github.com>
2023-05-15 07:15:18 +0300

    Add files via upload

---
+++ scripts/navbar.js
@@ -0,0 +1,14 @@
+var modal = document.getElementById("myModal");
+
+function openModal() {
+    modal.style.display = "block";
+}
+
+function closeModal() {
+    modal.style.display = "none";
+}
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
